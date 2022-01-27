9f5771c98e62d78e8a6453ac249fce8a31c3be2b
Raman Srinivasa Venkat <raman.venkat@in.abb.com>
2022-01-27 16:09:35 +0530

    Initial commit and push.

---
+++ src/azure-storage-account-operations/azure-storage-account-operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_storage_account_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
